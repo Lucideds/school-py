51e9192b6b42ba71f843c66eb0d8fab004eecc5b
leeam <lucideds@proton.me>
2024-10-19 23:18:21 +0100

    commit idk

--- Part 18/Task 1.py
+++ Part 18/Task 1.py
@@ -1,24 +1,26 @@
-from random import randint
+import random
 
-
-word = "" 
-
+word = ""
 
 word = word + input("First word: ")
 word = word + input("Second word: ")
 word = word + input("Third word: ")
-new_word = "" 
+
+new_word = ""
+
 for letter in word: 
+    print(letter)
     if letter == "a": 
-        new_word = new_word + chr(randint(33, 37))
+        new_word = new_word + chr(random.randint(33, 37))
     elif letter == "e":
-        new_word = new_word + chr(randint(38, 42))
+        new_word = new_word + chr(random.randint(38, 42))
     elif letter == "i":
-        new_word = new_word + chr(randint(43, 47))
+        new_word = new_word + chr(random.randint(43, 47))
     elif letter == "o":
-        new_word = new_word + chr(randint(58, 61))
+        new_word = new_word + chr(random.randint(58, 61))
     elif letter == "u":
-        new_word = new_word + chr(randint(91, 94))
+        new_word = new_word + chr(random.randint(91, 94))
     else: 
         new_word = new_word + letter 
+    print(new_word)
 print(new_word)  
